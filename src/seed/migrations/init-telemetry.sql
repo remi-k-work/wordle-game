aa1a5af89a3c22119fdff7315790024232e4eb62
Remi <remi.k.work@gmail.com>
2026-06-14 14:44:51 +0200

    switching from metric_value to metric_payload

--- src/seed/migrations/init-telemetry.sql
+++ src/seed/migrations/init-telemetry.sql
@@ -5,7 +5,7 @@ CREATE TABLE global_pulse (
 
   metric_name VARCHAR(50) NOT NULL,
   
-  metric_value INT NOT NULL,
+  metric_payload JSONB NOT NULL,
 
   solutions_lang VARCHAR(2) NOT NULL
     CONSTRAINT global_pulse_solutions_lang_check
@@ -18,14 +18,9 @@ CREATE TABLE global_pulse (
 CREATE INDEX global_pulse_time_series_idx 
   ON global_pulse (metric_name, solutions_lang, created_at DESC);
 
-INSERT INTO global_pulse (metric_name, metric_value, solutions_lang)
-VALUES
-  ('gamesPlayed', 1, 'En'),
-  ('validGuesses', 12, 'En'),
-  ('invalidGuesses', 2, 'Pl'),
-  ('openingGuesses', 1, 'En'),
-  ('timeToSolve', 14, 'En');
-
+--
+--
+--
 
 DROP TABLE IF EXISTS arcade_runs_summary;
 
@@ -61,11 +56,9 @@ CREATE TABLE arcade_runs_summary (
 CREATE INDEX arcade_runs_summary_leaderboard_idx 
   ON arcade_runs_summary (solutions_lang, final_score DESC, final_streak DESC);
 
-INSERT INTO arcade_runs_summary (run_id, death_reason, failed_on_word, final_score, final_streak, duration_seconds, solutions_lang)
-VALUES
-  ('run-abc-123', 'Guesses', 'XYLYL', 4200, 18, 450, 'En'),
-  ('run-def-456', 'Forfeit', 'N/A', 1500, 7, 120, 'Pl');
-
+--
+--
+--
 
 DROP TABLE IF EXISTS run_word_events;
 
@@ -95,10 +88,3 @@ CREATE TABLE run_word_events (
 -- Optimized for the "Drill-Down" query: SELECT * FROM run_word_events WHERE run_id = ? ORDER BY created_at ASC
 CREATE INDEX run_word_events_drill_down_idx 
   ON run_word_events (run_id, created_at ASC);
-
-INSERT INTO run_word_events (run_id, word, guessed_turn, time_seconds, solutions_lang)
-VALUES
-  ('run-abc-123', 'APPLE', 3, 12, 'En'),
-  ('run-abc-123', 'TRAIN', 1, 4, 'En'),
-  ('run-abc-123', 'MOUSE', 6, 45, 'En'),
-  ('run-def-456', 'KOTKI', 2, 8, 'Pl');
